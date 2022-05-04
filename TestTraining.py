4097e88cf649b9abd7930d38d44da680aa56d5ed
MaukWM <Hendrik.nl1>
2022-05-04 17:41:47 +0200

    :bug: Resolved major bug in forward pass

--- TestTraining.py
+++ TestTraining.py
@@ -11,15 +11,15 @@ class SineWave(Dataset):
 
     def __init__(self):
         self.dps = 500
-        self.x = np.random.uniform(-1, 1, self.dps)
-        self.y = np.sin(self.x)
+        self.x = torch.rand(self.dps, dtype=torch.float32)
+        self.y = torch.sin(self.x)
 
     def __len__(self):
         return self.dps
 
     def __getitem__(self, item):
         idx = np.random.randint(0, self.dps)
-        return torch.tensor([self.x[idx]]), torch.tensor([self.y[idx]])
+        return self.x[idx], self.y[idx]
 
 
 class Training:
@@ -29,31 +29,34 @@ class Training:
 
         _data_set = SineWave()
         # TODO: Fix batch sizes over 1
-        self.batch_size = 1
+        self.batch_size = 256
         self.data_generator = DataLoader(_data_set, batch_size=self.batch_size)
 
-        self.model = SparseNeuralNetwork(amount_hidden_layers=10, network_width=50)
-        self.model.double()
+        self.model = SparseNeuralNetwork(input_size=1, output_size=1, amount_hidden_layers=3, network_width=50)
+        # self.model.double()
 
     def train(self):
         self.model.train()
         criterion = nn.MSELoss()
-        optim = torch.optim.SGD(self.model.parameters(), lr=1e-5)
-
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=1e-5)
         for epoch in range(self.epochs):
-
             train_loss = 0
             i = 0
             with tqdm.tqdm(self.data_generator, unit="batch") as tepoch:
-                for inp_xs, true_ys in self.data_generator:
-                    optim.zero_grad()
+                for batch in self.data_generator:
+                    optimizer.zero_grad()
 
-                    inp_xs = torch.reshape(inp_xs, (self.batch_size, 1))
+                    inp_xs = torch.reshape(batch[0], (batch[0].size()[0], 1))
+                    true_ys = torch.reshape(batch[1], (batch[1].size()[0], 1))
 
-                    pred_y = self.model(inp_xs)
-                    loss = criterion(pred_y, true_ys)
+                    # print(inp_xs.shape)
+
+                    # print(f'training on {inp_xs} and {true_ys}')
+                    # print(inp_xs.dtype)
+                    pred_ys = self.model(inp_xs)
+                    loss = criterion(pred_ys, true_ys)
                     loss.backward()
-                    optim.step()
+                    optimizer.step()
 
                     # print statistics
                     i += 1
