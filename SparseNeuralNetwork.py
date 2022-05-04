13158f5c5b14b53dce49924be52e729a407f3234
MaukWM <maukmul@gmail.com>
2022-05-04 13:00:18 +0200

    :sparkles: Created Dense NN model with up to k depth activated skip connections

---
+++ SparseNeuralNetwork.py
@@ -0,0 +1,96 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import torch.nn.functional as F
+import numpy as np
+import random
+
+
+class SparseNeuralNetwork(nn.Module):
+
+    """
+    Skip depth of 1 means NO skip connections
+    """
+    def __init__(self, sparsity=0.5, max_connection_depth=4, amount_hidden_layers=3, network_width=3, input_size=1,
+                 output_size=1):
+        super(SparseNeuralNetwork, self).__init__()
+        # TODO: Check max_connection_depth not > network depth and check max_connection_depth=>1
+        self.sparsity = sparsity
+        self.max_connection_depth = max_connection_depth
+        self.amount_hidden_layers = amount_hidden_layers
+        self.network_width = network_width
+        self.input_size = input_size
+        self.output_size = output_size
+
+        self.layers = nn.ModuleDict()
+
+        self.initialize_network()
+
+        # print(self.layers)
+
+        # Depending on skip_depth we keep track of different lists of layers
+
+    def initialize_network(self):
+        for i in range(1, self.max_connection_depth + 1):
+            # print(f'creating {i} skip layer')
+            # TODO: This will not work on skip depths equal to network depth
+
+            # If i is larger (or equal) than the amount of hidden layers it's impossible to continue, so add a skip
+            # connection from start to end and break out of the loop, we're done!
+            if i > self.amount_hidden_layers:
+                # print(f'size 1')
+                self.layers[str(i)] = nn.ModuleList([nn.Linear(in_features=self.input_size, out_features=self.output_size)])
+                break
+
+            _layers = nn.ModuleList([nn.Linear(in_features=self.input_size, out_features=self.network_width)])
+
+            for j in range(self.amount_hidden_layers - i):
+                _layers.append(nn.Linear(in_features=self.network_width, out_features=self.network_width))
+
+            _layers.append(nn.Linear(in_features=self.network_width, out_features=self.output_size))
+            # print(f'size {len(_layers)}')
+            self.layers[str(i)] = _layers
+        # print(self.layers)
+
+    def forward(self, _x):
+        # TODO: Verify correctness of forward pass
+        # Depending on skip_depth we keep track of all previously calculated xs
+        # This is x_0 that is received by the first layer
+        _xs = {0: _x}
+
+        for i in range(self.amount_hidden_layers + 1):
+            # print(f'performing calculatios on layer{i}')
+            _new_x = 0
+            for j in range(0, i + 1):
+                # print(f'[{i + 1 - j}][{j}]')
+                # print(f"updating _new_x from {_new_x}", end=" ")
+
+                # Can't have more skip connection networks than max_connection_depth
+                if i + 1 - j > self.max_connection_depth:
+                    break
+
+                _new_x = _new_x + F.relu(self.layers[str(i + 1 - j)][j](_xs[j]))
+                # print(f"to {_new_x}")
+                # _new_x = _new_x + F.relu(self.layers[j](_xs[i - j]))
+            _xs[i + 1] = _new_x
+            # print(f'adding {_new_x} to {i + 1} key in _xs')
+
+        # print(_xs)
+        return _xs[len(_xs) - 1]
+
+    def __repr__(self):
+        return f"SparseNN={{sparsity={self.sparsity}, skip_depth={self.max_connection_depth}, network_depth={self.amount_hidden_layers}, " \
+               f"network_width={self.network_width}}}"
+
+
+if __name__ == "__main__":
+
+    input_size = 1
+    snn = SparseNeuralNetwork(input_size=input_size)
+
+    x = torch.rand(input_size)
+
+    print(snn(x))
+    # print(snn)
