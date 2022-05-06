e035042b3b1c3c534f999be30ec46fe56dc7a078
MaukWM <Hendrik.nl2>
2022-05-06 12:39:38 +0200

    :sparkles: Sparsity and sparsity ratio between sequential and skip can now be initialized in the network

--- SparseNeuralNetwork.py
+++ SparseNeuralNetwork.py
@@ -18,6 +18,8 @@ class SparseNeuralNetwork(nn.Module):
                  output_size=1, skip_sequential_ratio=0.5):
         super(SparseNeuralNetwork, self).__init__()
         # TODO: Check max_connection_depth not > network depth and check max_connection_depth=>1
+        # TODO: Dissallow ratio if max_conn_depth = 1
+        # TODO: dissallow sparsity=1
         # Set variables
         self.sparsity = sparsity
         self.max_connection_depth = max_connection_depth
@@ -35,11 +37,6 @@ class SparseNeuralNetwork(nn.Module):
         # print([name for name, _ in self.named_parameters() if name.split(".")[1] != "1" and "bias" not in name])
         self.n_skip_instances = len([name for name, _ in self.named_parameters() if name.split(".")[1] != "1" and "bias" not in name])
 
-        # Initialize mask for sparsity
-        self.masks = {}
-        self.initialize_mask()
-        self.apply_mask()
-
         # print("start")
         # for name, param in self.named_parameters():
         #     print(name, param)
@@ -49,8 +46,27 @@ class SparseNeuralNetwork(nn.Module):
         self.hidden_activation_function = F.relu
         self.final_activation_function = None
 
-        # Calculate max amount of sequential connections, to be used for calculating global sparsity target
+        # Calculate max amount of sequential and skip connections, to be used for calculating global sparsity target and
+        # in order to initialize the target ratio
         self.n_max_sequential_connections = self.calculate_n_max_sequential_connections()
+        self.n_max_skip_connections = self.calculate_n_max_skip_connections()
+
+        # Calculate target n active connections for sequential and skip networks
+        self.n_target_active_connections = self.n_max_sequential_connections - round(self.sparsity * self.n_max_sequential_connections)
+        self.n_target_sequential_connections = round(self.n_target_active_connections * self.skip_sequential_ratio)
+        self.n_target_skip_connections = round(self.n_target_active_connections * (1 - self.skip_sequential_ratio))
+
+        self.sequential_sparsity = 1 - self.n_target_sequential_connections / self.n_max_sequential_connections
+        self.skip_sparsity = 1 - self.n_target_skip_connections / self.n_max_skip_connections
+
+        print(f'Max seq con={self.n_max_sequential_connections}, Max skip con={self.n_max_skip_connections}')
+        print(f'Target act con={self.n_target_active_connections}, Target seq con={self.n_target_sequential_connections}, Target skip con={self.n_target_skip_connections}')
+        print(f'Target seq con sparsity={self.sequential_sparsity}, Target skip con sparsity={self.skip_sparsity}')
+
+        # Initialize mask for sparsity
+        self.masks = {}
+        self.initialize_mask()
+        self.apply_mask()
 
     def calculate_n_max_sequential_connections(self):
         result = self.input_size * self.network_width
@@ -59,6 +75,23 @@ class SparseNeuralNetwork(nn.Module):
             result += self.network_width * self.network_width
 
         result += self.network_width * self.output_size
+
+        return result
+
+    def calculate_n_max_skip_connections(self):
+        result = 0
+        for i in range(2, self.max_connection_depth + 1):
+
+            if i > self.amount_hidden_layers:
+                result += self.input_size * self.output_size
+                break
+
+            result += self.input_size * self.network_width
+
+            for j in range(self.amount_hidden_layers - i):
+                result += self.network_width * self.network_width
+
+            result += self.network_width * self.output_size
 
         return result
 
@@ -82,12 +115,13 @@ class SparseNeuralNetwork(nn.Module):
             else:
                 n_skip_connections += np.count_nonzero(self.masks[name])
 
-        overall_sparsity = (n_seq_connections + n_skip_connections) / self.n_max_sequential_connections
-        sequential_sparsity = n_seq_connections / self.n_max_sequential_connections
-        skip_sparsity = n_skip_connections / self.n_max_sequential_connections
-        sparsity_ratio = sequential_sparsity / (skip_sparsity + sequential_sparsity)
+        overall_sparsity = 1 - (n_seq_connections + n_skip_connections) / self.n_max_sequential_connections
+        sequential_sparsity = 1 - n_seq_connections / self.n_max_sequential_connections
+        skip_sparsity = 1 - n_skip_connections / self.n_max_sequential_connections
+        sparsity_ratio = n_seq_connections / (n_skip_connections + n_seq_connections)
 
-        print(f"[Sparsity] OverallSparsity={overall_sparsity}, SequentialSparsity={sequential_sparsity}, SkipSparsity={skip_sparsity}, SparsityRatio={sparsity_ratio},")
+        print(f"[Sparsity] N active connections={n_seq_connections + n_skip_connections}, N active seq connections={n_seq_connections}, N active skip connections={n_skip_connections}")
+        print(f"[Sparsity] OverallSparsity={overall_sparsity}, SequentialSparsity={sequential_sparsity}, SkipSparsity={skip_sparsity}, SparsityRatio={sparsity_ratio}")
 
         return overall_sparsity, sequential_sparsity, skip_sparsity, sparsity_ratio
 
@@ -101,13 +135,15 @@ class SparseNeuralNetwork(nn.Module):
 
     def initialize_mask(self):
         # First calculate the sparsity for each individual layer, as we must incorporate both global sparsity and
+        # We know the maximum amount of sequential and skip connections. We must simply uphold the ratio now
+        # individual_sequential_layer_sparsity = (self.n_max_sequential_connections * self.sparsity * self.skip_sequential_ratio) / self.n_max_sequential_connections
         # sparsity ratio
-        individual_sequential_layer_sparsity = self.sparsity * (1 - self.skip_sequential_ratio)
+        # individual_sequential_layer_sparsity = self.sparsity * (1 - self.skip_sequential_ratio)
 
         # We can't simply calculate one value for this right?
-        individual_skip_layer_sparsity = self.sparsity * self.skip_sequential_ratio / self.n_skip_instances
+        # individual_skip_layer_sparsity = self.sparsity * self.skip_sequential_ratio / self.n_skip_instances
 
-        print(individual_sequential_layer_sparsity, individual_skip_layer_sparsity)
+        # print(individual_sequential_layer_sparsity, individual_skip_layer_sparsity)
 
         # We need to calculate how many skip connection instances there will be of [layers.x.x.weights], this is not hard
         # Then we know how many initialization rounds there will be and we can calculate the probability each initialization round needs
@@ -120,10 +156,10 @@ class SparseNeuralNetwork(nn.Module):
 
             # If this is a layer named layers.1.x.weight, it is a sequential layer
             if name_split[1] == "1":
-                self.masks[name] = np.random.rand(*param.shape) > (1 - individual_sequential_layer_sparsity)
+                self.masks[name] = np.random.rand(*param.shape) > self.sequential_sparsity
             # If this is not a layer named layers.1.x.weight, it is a skip layer
             else:
-                self.masks[name] = np.random.rand(*param.shape) > individual_skip_layer_sparsity
+                self.masks[name] = np.random.rand(*param.shape) > self.skip_sparsity
 
     def initialize_network(self):
         # Each iteration of this loop initializes connections for connection depth i.
@@ -190,8 +226,8 @@ class SparseNeuralNetwork(nn.Module):
 if __name__ == "__main__":
 
     input_size = 1
-    snn = SparseNeuralNetwork(input_size=input_size, amount_hidden_layers=3, max_connection_depth=4, network_width=3,
-                              sparsity=0.5, skip_sequential_ratio=0.5)
+    snn = SparseNeuralNetwork(input_size=input_size, amount_hidden_layers=30, max_connection_depth=14, network_width=30,
+                              sparsity=0.8, skip_sequential_ratio=0)
 
     x = torch.rand((10, input_size))
 
