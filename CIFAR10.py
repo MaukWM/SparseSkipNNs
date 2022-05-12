5e6c1237f8445266d5ec41d09011aeae4ce834cf
MaukWM <Hendrik.nl2>
2022-05-12 14:51:02 +0200

    :sparkles: Added CIFAR10 dataset and made intial framework for extending to other datasets. Added accuracy tracking and visualization

---
+++ CIFAR10.py
@@ -0,0 +1,6 @@
+from torch.utils.data import Dataset, DataLoader, random_split
+
+# TODO: Each different dataset has a different class, so that we can generalize the plot_model_distribution etc.
+class CIFAR10(Dataset):
+
+    pass
--- item_keys.py
+++ item_keys.py
@@ -4,6 +4,8 @@ import enum
 class ItemKey(enum.Enum):
 
     VALIDATION_LOSS = "validation_loss"
+    VALIDATION_ACCURACY = "validation_accuracy"
     TRAINING_LOSS = "training_loss"
+    TRAINING_ACCURACY = "training_accuracy"
     SPARSITIES = "sparsities"
 
