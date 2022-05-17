7f82ca3f63a195c4dac6e5f8dddc94126c54b10c
MaukWM <maukmul@gmail.com>
2022-05-17 13:43:41 +0200

    :sparkles: Made sparsity tracking more dynamic Added utils

--- Visualizer.py
+++ Visualizer.py
@@ -1,10 +1,8 @@
-from abc import ABC
-
 import matplotlib.pyplot as plt
 import numpy as np
 
 import SparseTrainer
-from SparseNeuralNetwork import SparseNeuralNetwork
+import util
 from item_keys import ItemKey
 
 # TODO: Make this a drawer class with drawer components so that we can draw multiple subplots or draw in the same plot
@@ -22,7 +20,7 @@ class Visualizer:
 
         if self.trainer.evolution_interval is not None:
             self.plot_sparsity_info()
-            self.plot_k_distribution()
+            self.plot_k_distributions()
             self.plot_k_evolution_graphs()
 
     def plot_train_val_loss(self):
@@ -41,25 +39,16 @@ class Visualizer:
         # TODO: Split function into generalized method for 3 different distributions
         _xs = np.arange(start=0, stop=len(self.trainer_items[ItemKey.N_ACTIVE_CONNECTIONS.value]) * self.trainer.evolution_interval, step=self.trainer.evolution_interval)
 
-        k_n_dists_LD = self.trainer_items[ItemKey.K_N_DISTRIBUTION.value]
-        k_sparsity_dists_LD = self.trainer_items[ItemKey.K_SPARSITY_DISTRIBUTION.value]
-        k_sparsity_by_max_seq_dists_LD = self.trainer_items[ItemKey.K_SPARSITY_DISTRIBUTION_BY_MAX_SEQ.value]
-
-        # TODO: Generalize to util function
-        # List of Dicts to Dicts of Lists from: https://stackoverflow.com/questions/5558418/list-of-dicts-to-from-dict-of-lists
-        k_n_dists_DL = {k: [dic[k] for dic in k_n_dists_LD] for k in k_n_dists_LD[0]}
-        k_sparsity_dists_DL = {k: [dic[k] for dic in k_sparsity_dists_LD] for k in k_sparsity_dists_LD[0]}
-        k_sparsity_by_max_seq_dists_DL = {k: [dic[k] for dic in k_sparsity_by_max_seq_dists_LD] for k in k_sparsity_by_max_seq_dists_LD[0]}
-        # print(k_n_dists_DL)
-        # print(k_sparsity_dists_DL)
-        # print(k_sparsity_by_max_seq_dists_DL)
+        k_n_dists = util.ld_to_dl(self.trainer_items[ItemKey.K_N_DISTRIBUTION.value])
+        k_sparsity_dists = util.ld_to_dl(self.trainer_items[ItemKey.K_SPARSITY_DISTRIBUTION.value])
+        k_sparsity_by_max_seq_dists = util.ld_to_dl(self.trainer_items[ItemKey.K_SPARSITY_DISTRIBUTION_BY_MAX_SEQ.value])
 
         plt.title("N change by k")
         plt.xlabel("Epoch")
         plt.ylabel("N")
         plt.grid()
-        for k in k_n_dists_DL.keys():
-            plt.plot(_xs, k_n_dists_DL[k], label=f"k={k}")
+        for k in k_n_dists.keys():
+            plt.plot(_xs, k_n_dists[k], label=f"k={k}")
         plt.legend()
         plt.show()
 
@@ -68,8 +57,8 @@ class Visualizer:
         plt.xlabel("Epoch")
         plt.ylabel("Sparsity")
         plt.grid()
-        for k in k_n_dists_DL.keys():
-            plt.plot(_xs, k_sparsity_dists_DL[k], label=f"k={k}")
+        for k in k_n_dists.keys():
+            plt.plot(_xs, k_sparsity_dists[k], label=f"k={k}")
         plt.legend()
         plt.show()
 
@@ -78,17 +67,16 @@ class Visualizer:
         plt.xlabel("Epoch")
         plt.ylabel("Sparsity")
         plt.grid()
-        for k in k_n_dists_DL.keys():
-            plt.plot(_xs, k_sparsity_by_max_seq_dists_DL[k], label=f"k={k}")
+        for k in k_n_dists.keys():
+            plt.plot(_xs, k_sparsity_by_max_seq_dists[k], label=f"k={k}")
         plt.legend()
         plt.show()
 
-    def plot_k_distribution(self):
-        # TODO: Split function into generalized method for 3 different distributions
-        plt.title("Initial/Final K N Distribution")
-        k_n_dists = self.trainer_items[ItemKey.K_N_DISTRIBUTION.value]
-        final_k_n_dist = k_n_dists[len(k_n_dists) - 1]
-        initial_k_n_dist = k_n_dists[0]
+    @staticmethod
+    def plot_k_distribution(k_n_dist_values, plot_title):
+        plt.title(plot_title)
+        final_k_n_dist = k_n_dist_values[len(k_n_dist_values) - 1]
+        initial_k_n_dist = k_n_dist_values[0]
 
         plt.grid()
         plt.plot(final_k_n_dist.keys(), final_k_n_dist.values(), label="Final distribution")
@@ -96,27 +84,16 @@ class Visualizer:
         plt.legend()
         plt.show()
 
-        plt.title("Initial/Final K Sparsity Distribution By K Sparsity")
-        k_sparsity_dists = self.trainer_items[ItemKey.K_SPARSITY_DISTRIBUTION.value]
-        final_k_sparsity_dist = k_sparsity_dists[len(k_sparsity_dists) - 1]
-        initial_k_sparsity_dist = k_sparsity_dists[0]
+    def plot_k_distributions(self):
+        # TODO: Split function into generalized method for 3 different distributions
+        self.plot_k_distribution(k_n_dist_values=self.trainer_items[ItemKey.K_N_DISTRIBUTION.value],
+                                 plot_title="Initial/Final K N Distribution")
 
-        plt.grid()
-        plt.plot(final_k_sparsity_dist.keys(), final_k_sparsity_dist.values(), label="Final distribution")
-        plt.plot(initial_k_sparsity_dist.keys(), initial_k_sparsity_dist.values(), label="Initial distribution")
-        plt.legend()
-        plt.show()
+        self.plot_k_distribution(k_n_dist_values=self.trainer_items[ItemKey.K_SPARSITY_DISTRIBUTION.value],
+                                 plot_title="Initial/Final K Sparsity Distribution By K Sparsity")
 
-        plt.title("Initial/Final K Sparsity Distribution By Max Seq Sparsity")
-        k_sparsity_by_max_seq_dists = self.trainer_items[ItemKey.K_SPARSITY_DISTRIBUTION_BY_MAX_SEQ.value]
-        final_k_sparsity_by_max_seq_dist = k_sparsity_by_max_seq_dists[len(k_sparsity_by_max_seq_dists) - 1]
-        initial_k_sparsity_by_max_seq_dist = k_sparsity_by_max_seq_dists[0]
-
-        plt.grid()
-        plt.plot(final_k_sparsity_by_max_seq_dist.keys(), final_k_sparsity_by_max_seq_dist.values(), label="Final distribution")
-        plt.plot(initial_k_sparsity_by_max_seq_dist.keys(), initial_k_sparsity_by_max_seq_dist.values(), label="Initial distribution")
-        plt.legend()
-        plt.show()
+        self.plot_k_distribution(k_n_dist_values=self.trainer_items[ItemKey.K_SPARSITY_DISTRIBUTION_BY_MAX_SEQ.value],
+                                 plot_title="Initial/Final K Sparsity Distribution By Max Seq Sparsity")
 
     def plot_accuracies(self):
         plt.title("Training and validation accuracies")
